ddf1f5c91c9401385496d213ff871647bf203428
Mario Clavijo <clavijomarioa@outlook.com>
2023-04-01 22:05:36 -0500

    Finished NAV. Working on About section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {RiAwardFill} from 'react-icons/ri'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+      </div>
+
+      <div className='about__content'>
+        <div className='about__cards'>
+          <article className='about_card'>
+            <RiAwardFill className='about__icon'/>
+            <h5>Experience</h5>
+            <small>3+ Years of experience</small>
+          </article>
+
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import './nav.css'
+import React from "react";
+import "./nav.css";
+import { GoHome } from "react-icons/go";
+import { BiUserCircle } from "react-icons/bi";
+import { GiClassicalKnowledge } from "react-icons/gi";
+import { MdOutlineDesignServices, MdOutlineMessage } from "react-icons/md";
+import { useState } from "react";
 
 function Nav() {
+  const [activeNav, setActiveNav] = useState("#");
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <GoHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        {" "}
+        <BiUserCircle />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <GiClassicalKnowledge />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <MdOutlineDesignServices />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <MdOutlineMessage />
+      </a>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
